7d38d82034354864f2736abbcbf0784c55fb7184
Johannes Drechsler <jonjohnsonwright@gmail.com>
2017-03-23 16:27:25 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+	<div id="app" class="container-fluid">
+		<img src="./assets/logo.png">
+
+		<div class="row">
+			<div class="col-md-12">
+				<ManageProducts></ManageProducts>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<SocketTest></SocketTest>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue'
+	import Component from 'vue-class-component'
+	import ManageProducts from './components/ManageProducts.vue'
+	import SocketTest from './components/SocketTest.vue'
+
+	@Component({
+		components: {
+			ManageProducts,
+			SocketTest
+		}
+	})
+
+	export default class App extends Vue {
+
+	}
+
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #2c3e50;
+		background-color: mintcream;
+	}
+</style>
